938013b3bd49c1bf66b65927bd663d834b2e7a4d
Marina <kikkafikka90@gmail.com>
2022-07-06 21:15:32 +0400

    db done

---
+++ db/migrations/20220706160753-create-user.js
@@ -0,0 +1,45 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      secret: {
+        type: Sequelize.TEXT,
+        unique: true,
+      },
+      pass: {
+        type: Sequelize.TEXT,
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Roles',
+          },
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
