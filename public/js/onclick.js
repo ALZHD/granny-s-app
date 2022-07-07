cf16c68dbff9ae97e4f97e1a221eb7be8079dccf
ALZHD <alexanderzhdanovich95@gmail.com>
2022-07-07 19:21:44 +0300

    Smth fixed

--- public/js/onclick.js
+++ public/js/onclick.js
@@ -16,12 +16,13 @@ const button = document.querySelector('.btn btn-primary');
 // const x = document.forms[0].form1;
 
 const x = document.forms.form1;
+const y = document.forms.form2;
+
 console.log(x);
 
 x.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const allformFrom = Object.from(new FormData(x));
-  console.log(x);
+  const allformFrom = Object.fromEntries(new FormData(x));
   const response = await fetch('http://localhost:3000/signup', {
     method: 'post',
     headers: { 'Content-type': 'application/json' },
@@ -29,11 +30,32 @@ x.addEventListener('submit', async (e) => {
   });
   const result = await response.json();
 
-  if (response.status === 501) {
-    alert(`ошибка:${result.isFail}`);
-  } else if (response.ok) {
-    window.location('/main');
+  if (response.ok) {
+    console.log(1);
+    window.location.href = '/main';
+  } else {
+    console.log(result);
+    // alert(`ошибка:${result.isFail}`);
   }
 });
 
+y.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const allformFrom = Object.fromEntries(new FormData(y));
+  const response = await fetch('http://localhost:3000/signin', {
+    method: 'post',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(allformFrom),
+  });
+  const result = await response.json();
+
+  if (response.ok) {
+    console.log(1);
+    window.location.href = '/main';
+  } else {
+    console.log(result);
+    // alert(`ошибка:${result.isFail}`);
+  }
+});
 // buttonRadio.addEventListener('click');
+ 
